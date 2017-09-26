25e868822985046d137ef811708512ae895f7082
Filippo Bianchi <filippo.m.bianchi@uit.no>
2017-09-26 10:53:57 +0200

    removed files

--- xxx.py
+++
@@ -1,5 +0,0 @@
-from TS_datasets import getBlood
-
-(train_data, train_labels, train_len, train_targets, K_tr,
-        valid_data, valid_labels, valid_len, valid_targets, K_vs,
-        test_data, test_labels, test_len, test_targets, K_ts) = getBlood()
